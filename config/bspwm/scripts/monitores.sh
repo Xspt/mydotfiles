04d7cf55e4f725bf1bc6d7397f2c62616812fdb1
Xspt <82736724+Xspt@users.noreply.github.com>
2022-03-27 09:41:21 -0300

    Update monitores.sh

--- config/bspwm/scripts/monitores.sh
+++ config/bspwm/scripts/monitores.sh
@@ -5,8 +5,8 @@ tv=$(xrandr -q | grep HDMI-0)
 
 if [[ $old_monitor == *connected* ]] && [[ $tv == *connected* ]]; then
 	xrandr --output HDMI-0 --primary --mode 1366x768 --rotate normal --output VGA-0 --mode 1280x720 --rotate normal --below HDMI-0
-else if [[ $old_monitor == *connected* ]]; then
+elif [[ $old_monitor == *connected* ]]; then
 	xrandr --output VGA-0 --mode 1280x720 --rotate normal
-else if [[ $tv == *connected* ]]; then
+elif [[ $tv == *connected* ]]; then
 	xrandr --output HDMI-0 --mode 1366x768 --rotate normal
 fi
